b263cc52628e82cce3e97e42e598a7891cd9f9de
MalakiCarstens <Fireboymalaki@gmail.com>
2025-08-09 19:10:53 -0500

    MAUI and LAB 19

---
+++ MAUITutorials/CH15/CRUDUsers/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CRUDUsers.Models;
+
+namespace CRUDUsers.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
